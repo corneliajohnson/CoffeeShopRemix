9e014203be016f034b167dbb0bb8cbec0d8a94e3
corneliajohnson <corneliaj91@gmail.com>
2021-08-10 20:41:35 -0500

    added cors

--- CoffeeShopRemix/Startup.cs
+++ CoffeeShopRemix/Startup.cs
@@ -44,6 +44,14 @@ namespace CoffeeShopRemix
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CoffeeShopRemix v1"));
+
+                // Do not block requests while in development
+                app.UseCors(options =>
+                {
+                    options.AllowAnyOrigin();
+                    options.AllowAnyMethod();
+                    options.AllowAnyHeader();
+                });
             }
 
             app.UseHttpsRedirection();
