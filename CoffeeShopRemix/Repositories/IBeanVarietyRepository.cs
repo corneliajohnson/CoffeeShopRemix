216b775798d9b7ed08ae065ba4be17e1f0a6bbd4
corneliajohnson <corneliaj91@gmail.com>
2021-08-10 20:01:13 -0500

    first commit

---
+++ CoffeeShopRemix/Repositories/IBeanVarietyRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoffeeShopRemix.Models;
+using System.Collections.Generic;
+
+namespace CoffeeShopRemix.Repositories
+{
+    public interface IBeanVarietyRepository
+    {
+        void Add(BeanVariety variety);
+        void Delete(int id);
+        BeanVariety Get(int id);
+        List<BeanVariety> GetAll();
+        void Update(BeanVariety variety);
+    }
+}
